735feec8ba47abfe4d09e4f2f30c73069a0559e6
padraic <padraic@42ebe9d5-df93-401e-be29-8bd77c29fff2>
2009-01-28 22:44:20 +0000

    Non-tested methods added (simple getters/mutators)
    git-svn-id: https://padraic.unfuddle.com/svn/padraic_zf@15 42ebe9d5-df93-401e-be29-8bd77c29fff2

--- trunk/library/Zend/Controller/Action/Helper/Cache.php
+++ trunk/library/Zend/Controller/Action/Helper/Cache.php
@@ -1,7 +1,14 @@
 <?php
 
-/** Zend_Controller_Action_Helper_Abstract */
+/**
+ * @see Zend_Controller_Action_Helper_Abstract
+ */
 require_once 'Zend/Controller/Action/Helper/Abstract.php';
+
+/**
+ * @see Zend_Controller_Action_Exception
+ */
+require_once 'Zend/Controller/Action/Exception.php';
 
 /** Zend_Controller_Action_Helper_Abstract */
 require_once 'Zend/Cache/Manager.php';
@@ -9,11 +16,12 @@ require_once 'Zend/Cache/Manager.php';
 class Zend_Controller_Action_Helper_Cache extends Zend_Controller_Action_Helper_Abstract
 {
 
+    /**
+     * Local Cache Manager object used by Helper
+     *
+     * @var Zend_Cache_Manager
+     */
     protected $_manager = null;
-
-    public function init()
-    {
-    }
 
     public function direct()
     {
@@ -27,6 +35,37 @@ class Zend_Controller_Action_Helper_Cache extends Zend_Controller_Action_Helper_
     {
     }
 
+    /**
+     * Set an instance of the Cache Manager for this helper
+     *
+     * @param Zend_Cache_Manager $manager
+     * @return void
+     */
+    public function setManager(Zend_Cache_Manager $manager)
+    {
+        $this->_manager = $manager;
+    }
+
+    /**
+     * Get the Cache Manager instance or instantiate the object is not exists
+     *
+     * @return Zend_Cache_Manager
+     */
+    public function getManager()
+    {
+        if (is_null($this->_manager)) {
+            $this->_manager = new Zend_Cache_Manager;
+        }
+        return $this->_manager;
+    }
+
+    /**
+     * Proxy non-matched methods back to Zend_Cache_Manager where appropriate
+     *
+     * @param string $method
+     * @param array $args
+     * @return mixed
+     */
     protected function __call($method, $args)
     {
         if (method_exists($method, $this->_manager)) {
